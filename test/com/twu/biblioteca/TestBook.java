f5284231012a10e9a2754cbd4e744adb3eb512ee
jing sun <jing.sun@rea-group.com>
2016-04-23 13:03:14 +0800

    add test for Book

---
+++ test/com/twu/biblioteca/TestBook.java
@@ -0,0 +1,26 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by sunjing on 4/23/16.
+ */
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestBook {
+    @Test
+    public void Test() {
+        Book book = new Book("doudou", "2016-04");
+        assertEquals("doudou", book.getName());
+        assertEquals("2016-04", book.getYear_published());
+
+        book.setName("sunjing");
+        assertEquals("sunjing", book.getName());
+
+        book.setYear_published("2016-05");
+        assertEquals("2016-05",book.getYear_published());
+
+        book.setIscheckout(true);
+        assertEquals(true,book.ischeckout());
+    }
+}
